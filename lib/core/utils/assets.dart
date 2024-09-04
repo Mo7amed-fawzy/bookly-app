adfd4cab3c7efaa041ec0c8072b54e34e3265ff6
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-09-04 19:50:34 +0200

    add custom list view itmem to home

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,4 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
+  static const testImage1 = 'assets/images/test_image1.png';
 }
---
+++ lib/features/home/presentation/views/widgets/custom_list_view_intem.dart
@@ -0,0 +1,31 @@
+import 'package:flutercoursetwo/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: AspectRatio(
+        // الويدجيت ظبطت ابعاد الكونتانر بناءا علي الويدث بتاع السايزدبكوس
+        aspectRatio: 2.7 / 4,
+        // width / hight
+        child: Container(
+          // width: 100,
+          // height: MediaQuery.of(context).size.height * .25,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.red,
+              image: const DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  AssetsData.testImage1,
+                ),
+              )),
+        ),
+      ),
+    );
+  }
+}

c762b21785bfebde65a0e915c340351314316cb1
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-09-09 16:36:23 +0200

    create book_datails_view.dart

---
+++ lib/features/home/presentation/views/widgets/book_datails_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// احنا مثبتين البريفكس مع اسم الفايل واسم الويدجت فالتسميه الوهو هنا فيو
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}

9e2e88fed6489523d85dc6a1f062835dbc63089b
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-09-09 14:37:53 +0200

    best seller Slivers done

---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutercoursetwo/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+  // هعمل كوستوم سكرولفيو وبتعتمد علي حاجه اسمها سليفرز الهي بتخلي مجموعة ويدجتس تسكرول مع بعض
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      // دا احسن حاجه تستعملها لليستس العاوزه سكرول
+      // ودي البقول بيها هسكرول ف ايه
+      // SliverChildListDelegate كان في دي برضه بس دي للحجات الصغيره المحدوده
+      delegate: SliverChildBuilderDelegate(
+        // مسؤول عن بناء العناصر الخاصة بالـ سليفر بطريقة ديناميكية الهي طريقة بنا العناصر وبتبني الفلشاشه بس
+        childCount: 10, // عدد العناصر
+        (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: BestSallerListViewItem(),
+          );
+        },
+      ),
+    );
+  }
+}
+// كود ثروت سامي القديم
+    // شلت هنا اكسبانديد عشان مينفعش يكون في 2 اسبانديد
+//     return ListView.builder(
+//       // بتبني الويدجيتس البتظهر علي السكرين عشان كدا الهايت بتاعها بياكسباند براحتو ولكن استعملت شرنك راب بتروو عشان تقدر تحدد الارتفاع بتاعها وبتالي استعملت فيزكس نوتسكرولابل واحطها جوا حاجه بتسكرول
+//       // shrinkWrap: true, // عاوزك تاخد الهايت بتاع التشيلدرن بتوعك
+//       physics:
+//           const NeverScrollableScrollPhysics(), // هنا مربط الفرس بخليها متسكرولش
+//       padding: EdgeInsets.zero, // بشيل البادنج الديفولت
+//       itemCount: 10,
+//       itemBuilder: (context, index) {
+//         return const Padding(
+//           padding: EdgeInsets.symmetric(vertical: 10),
+//           child: BestSallerListViewItem(),
+//         );
+//       },
+//     );
+//   }
+// }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,27 +1,41 @@
 import 'package:flutercoursetwo/core/utils/styles.dart';
-import 'package:flutercoursetwo/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:flutercoursetwo/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:flutercoursetwo/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutercoursetwo/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppBar(),
-        FeatureBoxListView(),
-        Padding(
-          padding: EdgeInsets.only(left: 30, bottom: 20),
-          child: Text(
-            'Best seller',
-            style: Styles.textStyle18,
-          ),
-        ),
-        BestSallerListViewItem(),
+    // بشكل عام بتعمل نسيتيد سكرولفيو لكل الصفحه شكلها جامد
+    return const CustomScrollView(
+      // دي لاي حاجه عاوزها تسكرول هوريزونتالي
+      slivers: [
+        // هنا مينفعش احط ويدجتس لازم احط سليفر فهحط البروبيرتي دي
+        SliverToBoxAdapter(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomAppBar(),
+            FeatureBoxListView(),
+            Padding(
+              padding: EdgeInsets.only(left: 30, bottom: 20),
+              child: Text(
+                'Best seller',
+                style: Styles.textStyle18,
+              ),
+            ),
+            // BestSellerListView(),
+          ],
+        )),
+        // SliverFillRemaining(
+        //   // بالسليفر دا بقولو التشايلد دا هيملا الجزء الفاضل فبكدا بياكسباند عادي اقدر احط اكسبانديد
+        //   child:
+        BestSellerListView(), // في مشكلة اني مش عاوز يعمل 2 سكرول فهنزل احط شوية اتربيوتس
+        // ),
       ],
     );
   }

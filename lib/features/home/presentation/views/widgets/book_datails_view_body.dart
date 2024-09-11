ef98cabe7485bed74f43a01290cf6b4a06394d80
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-09-11 19:48:06 +0200

    add custom scroll view

--- lib/features/home/presentation/views/widgets/book_datails_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_datails_view_body.dart
@@ -6,6 +6,7 @@ import 'package:flutercoursetwo/features/home/presentation/views/widgets/custom_
 import 'package:flutercoursetwo/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 // احنا مثبتين البريفكس مع اسم الفايل واسم الويدجت فالتسميه الوهو هنا فيو
@@ -17,61 +18,79 @@ class BookDetailsViewBody extends StatelessWidget {
     // هستعمل حاجه ريلاتف للويدث
     // عشان تحافظلي علي الويدث
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 30,
-      ),
-      child: Column(
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const CustomBookDetailAppBar(),
-          Padding(
-            // امته استخدم قيمه ثابته وامته استخدم قيمه ريلاتف للويدث او الهايت
-            // لما اكون عاوز احافظ علي الشكل ذات نفسه ولما اكون محفتاجها بحيث العناصر متبعدش عن بعض وانا عاوزهم جمب بعض
-            padding: EdgeInsets.symmetric(horizontal: width * .17),
-            child: const CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 43,
-          ),
-          Text('The Jungle Book',
-              style: Styles.textStyle30.copyWith(fontWeight: FontWeight.normal
-                  // FontWeight.bold
-                  )),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: .7,
-            child: Text('Rudyard Kipling',
-                style: Styles.textStyle18.copyWith(
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500,
+
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          // علشان الكوستومسكرولفيو بتسكرول عادي فلما تنزل لجوا مش لازم تبقي سكرولابل
+          hasScrollBody: false,
+          // بقلو املا الفراغات
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30,
+            ),
+            // child: SingleChildScrollView(
+            // دي بتشرنك بتاخد زي الكولمن لما بقولو مايناكسسالايمنت دوت مين بيشرنك حولين التشلدرن بتوعو
+            // وحاطط فنفس الوقت اكسبانديد ويدجيت فكدا بقولو خد التقدر عليه والتانيه بتقولو عايزك تشرينك
+
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CustomBookDetailAppBar(),
+                Padding(
+                  // امته استخدم قيمه ثابته وامته استخدم قيمه ريلاتف للويدث او الهايت
+                  // لما اكون عاوز احافظ علي الشكل ذات نفسه ولما اكون محفتاجها بحيث العناصر متبعدش عن بعض وانا عاوزهم جمب بعض
+                  padding: EdgeInsets.symmetric(horizontal: width * .17),
+                  child: const CustomBookImage(),
+                ),
+                const SizedBox(
+                  height: 43,
+                ),
+                Text('The Jungle Book',
+                    style: Styles.textStyle30
+                        .copyWith(fontWeight: FontWeight.normal
+                            // FontWeight.bold
+                            )),
+                const SizedBox(
+                  height: 6,
+                ),
+                Opacity(
+                  opacity: .7,
+                  child: Text('Rudyard Kipling',
+                      style: Styles.textStyle18.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500,
+                      )),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(height: 30),
+                const BooksAction(),
+                const Expanded(
+                    child: SizedBox(
+                  height: 30,
                 )),
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(height: 30),
-          const BooksAction(),
-          const SizedBox(height: 39),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(
-                fontWeight: FontWeight.w600,
-              ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle14.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const SimilarBooksListView(),
+                // const SizedBox(height: 40),
+              ],
             ),
           ),
-          const SizedBox(height: 16),
-          const SimilarBooksListView(),
-          // const SizedBox(height: 40),
-        ],
-      ),
+        )
+      ],
     );
   }
 }

105fbdca31d64fe90e62740171e60de9490708e6
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-09-12 21:06:20 +0200

    create search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:flutercoursetwo/features/home/presentation/views/book_details_view.dart';
 import 'package:flutercoursetwo/features/home/presentation/views/home_view.dart';
+import 'package:flutercoursetwo/features/search/presentation/views/search_view.dart';
 import 'package:flutercoursetwo/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/BookDetailsView';
+  static const kSearchView = '/searchView';
 
   static final myroutes = GoRouter(
     routes: [
@@ -14,11 +16,15 @@ abstract class AppRouter {
         builder: (context, state) => const SplashView(),
       ),
       GoRoute(
-        path: '/homeView',
+        path: kSearchView, // نقطة البداية
+        builder: (context, state) => const SeacrchView(),
+      ),
+      GoRoute(
+        path: kHomeView,
         builder: (context, state) => const HomeView(),
       ),
       GoRoute(
-        path: '/BookDetailsView',
+        path: kBookDetailsView,
         builder: (context, state) => const BookDetailView(),
       ),
     ],

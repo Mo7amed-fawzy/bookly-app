49c0b5c7b55ab174428b70f23be62a9cdd54538f
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-09-14 19:23:59 +0200

    ceate home repo methods and errors file

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -2,7 +2,13 @@
 // هستعمل ريبوزتوري باترن بيعتمد علي ابستراكت كلاس وفيه الميثودس بتعتي بدون امبلمنتاشن => مقولش بيحصل فيها اي ) عشان الابستراكت
 // الريبوزتري باترن بحدد الفيتشر هتعمل اي لكن مش هتعملو ازاي
 // طبعا اقدر معملش كدا واعمل سيرفيز فيها الفنكشن كامله بس الريبو احسن
+import 'package:dartz/dartz.dart';
+import 'package:flutercoursetwo/core/errors/failure.dart';
+import 'package:flutercoursetwo/features/home/data/models/book_model.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  // هتريتيرن لليست اوف بوكمودل
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  // نوع تايب في 2 تايب الرايت ساكسس والليفت فيليار
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }

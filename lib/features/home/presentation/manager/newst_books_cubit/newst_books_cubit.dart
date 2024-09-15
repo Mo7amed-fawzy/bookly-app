fddeec849f4a2a2f0c3de2364f4a90ee469bc575
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-09-15 17:11:19 +0200

    newest books cubit done

--- lib/features/home/presentation/manager/newst_books_cubit/newst_books_cubit.dart
+++ lib/features/home/presentation/manager/newst_books_cubit/newst_books_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutercoursetwo/features/home/data/models/book_model.dart';
+import 'package:flutercoursetwo/features/home/data/repos/home_repo.dart';
 
 part 'newst_books_state.dart';
 
 class NewstBooksCubit extends Cubit<NewstBooksState> {
-  NewstBooksCubit() : super(NewstBooksInitial());
+  NewstBooksCubit(this.homeRepo) : super(NewstBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewstBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+
+    result.fold((failure) {
+      emit(NewstBooksFailure(failure.errMessage));
+    }, (books) // ددي حالة الساكسيس
+        {
+      emit(NewstBooksSuccess(books));
+    });
+  }
 }
